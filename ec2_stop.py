66aedd5a3746e1c84685cd90ad21d3a86c8620a5
alexg84 <galperin25@gmail.com>
2018-03-31 21:35:33 +0300

    Create ec2_stop.py

---
+++ ec2_stop.py
@@ -0,0 +1,19 @@
+import boto3
+
+client = boto3.client('ec2')
+
+def lambda_handler(event, context):
+    
+    response = client.describe_instances()
+    
+    for reservation in response["Reservations"]:
+        
+        for instance in reservation["Instances"]:
+            
+            print(instance["InstanceId"] + " " "stoping")
+
+            id = [instance["InstanceId"]]
+
+            client.stop_instances(InstanceIds=id)
+
+    return("Complited")
